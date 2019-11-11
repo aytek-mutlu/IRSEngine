7d3765cdb11a2a84b423989fe5dae84e7316c8d1
aytek-mutlu <55635311+aytek-mutlu@users.noreply.github.com>
2019-11-11 13:51:43 +0100

    Add files via upload

--- IRS_engine.py
+++ IRS_engine.py
@@ -14,8 +14,29 @@ import scipy.optimize as opt
 
 
 class Curve:
+    '''
+    Curve object: 
+        Object accepts swap yield curve with respective days to maturity 
+        and its compounding frequency as input.
+        
+        Assumptions:
+            Day count: 30/360
+            Ignore holidays, weekends
+        
+        Object attributes:
+            yield_curve
+            zero_curve
+            fwd_curve
+            discount_factor
+            
+        Object methods:
+            Interpolate:      Linear interpolation of broken days from a given curve
+            Curve2Discount:   Conversion of any given zero curve to discount factors
+            CurveShift:       Curve shift of any given Curve object by specified basis points
+    '''
     
     def __init__(self, curve_df,compound_freq):
+        
         self.curve_df = curve_df
         self.max_dtm = curve_df.Dtm.max()
         self.compound_freq = compound_freq
@@ -72,8 +93,30 @@ class Curve:
         return Curve(curve_df,self.compound_freq)
 
 class IRS:
+    '''
+    Curve object: 
+        Object accepts forward curve, discount curve, initial notional, start and end dates, 
+        amortisation type, amortisation schedule if needed as input
+        
+        Object attributes:
+            fwd_curve
+            discount_curve
+            notional
+            today
+            start_date
+            end_date
+            amortisation_type
+            date_schedule
+            num_of_payments
+            amortisation_schedule
+            
+        Object methods:
+            CalculateValue:      Calculation of present value of any given already issued swap
+            CalculatePar:        Calculation of par value of any given new swap
+    '''
     
     def __init__(self,fwd_curve,discount_curve,notional,today,start_date,end_date,amortisation_type='Constant',amortisation_schedule=None):
+        
         self.fwd_curve= fwd_curve
         self.discount_curve = discount_curve
         self.notional  = notional
@@ -84,6 +127,7 @@ class IRS:
         
                 
         def ScheduleGenerator(self):
+            
             schedule = list()
             schedule.append(self.start_date)
             new_date = self.start_date + relativedelta(months=3)
@@ -93,17 +137,22 @@ class IRS:
             return schedule
         
         self.date_schedule = ScheduleGenerator(self)
-        self.num_of_payments = len(self.date_schedule)-1    
+        self.num_of_payments = len(self.date_schedule)-1
         
         if self.amortisation_type =='Constant':
             self.amortisation_schedule = [notional for i in range(self.num_of_payments)]
         elif self.amortisation_type =='Linear':
             self.amortisation_schedule = [notional - (i/self.num_of_payments)*notional for i in range(self.num_of_payments)]
         elif self.amortisation_type =='Custom':
-            self.amortisation_schedule  = amortisation_schedule
+            if len(amortisation_schedule)==self.num_of_payments:
+                self.amortisation_schedule  = amortisation_schedule
+            else:
+                print('Amortisation schedule is not suitable with the given swap parameters. Linear amortisation is applied instead')
+                self.amortisation_schedule = [notional - (i/self.num_of_payments)*notional for i in range(self.num_of_payments)]
 
             
     def CalculateValue(self,par):
+    
         df = pd.DataFrame(index = range(self.num_of_payments),columns=['Period_Start','Period_End','Dtm','Notional','Reset_Rate','Zero_Rate','Discount_Factor',
                                    'Interest_Payment_Float','Payment_PV','Fixed_Rate','Fixed_Payment',
                                    'Fixed_Payment_PV'])
@@ -128,10 +177,7 @@ class IRS:
         
         r0=1
         pv = lambda r: self.CalculateValue(r)
-        #res= opt.minimize(pv, r0, method="BFGS",options={'gtol': 1e-2})
-        res= opt.root(pv, r0, method="hybr")
-        #print(res)
-        
+        res= opt.root(pv, r0, method="hybr")        
         return res.x[0]
     
         
@@ -140,30 +186,54 @@ class IRS:
 def main():
     today= pd.to_datetime('2019/11/04',dayfirst=False)
     
-    #read libor curve
-    libor_df = pd.read_csv('libor.csv')
+    #read curves
+    us_libor_df = pd.read_csv(r'Curves/US_LIBOR.csv')
+    us_ois_df = pd.read_csv(r'Curves/US_OIS.csv')
+    euribor_df = pd.read_csv(r'Curves/EURIBOR.csv')
+    eur_ois_df = pd.read_csv(r'Curves/EUR_OIS.csv')    
     
-    #Libor curve object
-    Libor = Curve(libor_df,compound_freq = 180)
+    #curve objects
+    US_Libor = Curve(us_libor_df,compound_freq = 180)
+    US_OIS = Curve(us_ois_df,compound_freq = 1)
+    Euribor = Curve(euribor_df,compound_freq = 180)
+    EUR_OIS = Curve(eur_ois_df,compound_freq = 1)    
     
-    #fwd_starting 1yr IRS with linear amortisation
-    start_date = pd.to_datetime('2019/12/01',dayfirst=False)
+    #already issued swap with 1.5 years of remaining maturity
+    start_date = pd.to_datetime('2019/06/01',dayfirst=False)
     end_date =  pd.to_datetime('2020/12/01',dayfirst=False)
-    usd_irs1 = IRS(Libor,Libor,10000000,today,start_date,end_date,'Linear')
-    usd_irs1_price = usd_irs1.CalculatePar()
+    usd_irs1 = IRS(US_Libor,US_OIS,10000000,today,start_date,end_date,amortisation_type='Constant')
+    usd_irs1_value = usd_irs1.CalculateValue(par=3)
     
-    #fwd_starting 20yr IRS with no amortisation
-    start_date = pd.to_datetime('2019/12/01',dayfirst=False)
-    end_date =  pd.to_datetime('2039/12/01',dayfirst=False)
-    usd_irs2 = IRS(Libor,Libor,10000000,today,start_date,end_date,'Constant')
-    usd_irs2_price = usd_irs2.CalculatePar()
+    #already issued swap with 1.5 years of remaining maturity with custom amortisation schedule
+    start_date = pd.to_datetime('2019/06/01',dayfirst=False)
+    end_date =  pd.to_datetime('2021/06/01',dayfirst=False)
+    notional  = 10000000
+    schedule = [notional,notional*7/8,notional*3/4,notional*5/8,notional*1/2,
+                notional*3/8,notional*1/4,notional*1/8]
+    usd_irs2 = IRS(Libor,Libor,notional,today,start_date,end_date,amortisation_type='Custom',
+                   amortisation_schedule=schedule)
+    usd_irs2_value = usd_irs2.CalculateValue(par=3)
     
-    #already started 20yr IRS with no amortisation
+    
+    #spot starting swap with 5 years of maturity and linear amortisation
+    start_date = pd.to_datetime('2019/11/06',dayfirst=False)
+    end_date =  pd.to_datetime('2024/11/06',dayfirst=False)
+    usd_irs3 = IRS(Libor,Libor,10000000,today,start_date,end_date,amortisation_type='Linear')
+    usd_irs3_price = usd_irs3.CalculatePar()
+    
+   
+    
+    #already started 20yr IRS with no amortisation and fixed rate of 2.1%
     start_date = pd.to_datetime('2018/12/01',dayfirst=False)
     end_date =  pd.to_datetime('2039/12/01',dayfirst=False)
-    usd_irs3 = IRS(Libor,Libor,10000000,today,start_date,end_date,'Constant')
-    usd_irs3_price = usd_irs3.CalculatePar()
-    usd_irs3_value = usd_irs3.CalculateValue(2.1)
+    usd_irs4 = IRS(Libor,Libor,10000000,today,start_date,end_date,'Constant')
+    usd_irs4_value = usd_irs4.CalculateValue(2.1)
+    
+    Libor_100_up  = Libor.CurveShift(100)
+    Libor_100_down  = Libor.CurveShift(-100)
+    
+    usd_irs4_price_delta_up  = IRS(Libor_100_up,Libor,10000000,today,start_date,end_date,'Constant').CalculateValue(2.1)- usd_irs4_value
+    usd_irs4_price_delta_down  = IRS(Libor_100_down,Libor,10000000,today,start_date,end_date,'Constant').CalculateValue(2.1)- usd_irs4_value    
 
 ### start main
 if __name__ == "__main__":
